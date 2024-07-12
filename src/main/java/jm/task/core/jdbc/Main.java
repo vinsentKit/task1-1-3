a310097a2c6f9ffd2b329a5b682b8c5498fdc0d4
Kater Katerok <k.sadoyakova21@gmail.com>
2024-07-12 21:50:14 +0300

    ready task JDBC.2

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,13 +1,38 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
-        /*  Создание таблицы User(ов)
-            Добавление 4 User(ов) в таблицу с данными на свой выбор.
-            После каждого добавления должен быть вывод в консоль (User с именем – name добавлен в базу данных)
-            Получение всех User из базы и вывод в консоль (должен быть переопределен toString в классе User)
-            Очистка таблицы User(ов)
-            Удаление таблицы */
+        UserServiceImpl userService = new UserServiceImpl();
+
+        // Создание таблицы User(ов)
+        userService.createUsersTable();
+
+        // Добавление 4 User(ов) в таблицу с данными на свой выбор
+        userService.saveUser("John", "Doe", (byte) 30);
+        System.out.println("User с именем – John добавлен в базу данных");
+
+        userService.saveUser("Jane", "Doe", (byte) 25);
+        System.out.println("User с именем – Jane добавлен в базу данных");
+
+        userService.saveUser("Alice", "Smith", (byte) 35);
+        System.out.println("User с именем – Alice добавлен в базу данных");
+
+        userService.saveUser("Bob", "Johnson", (byte) 28);
+        System.out.println("User с именем – Bob добавлен в базу данных");
+
+        // Получение всех User из базы и вывод в консоль
+        for (User user : userService.getAllUsers()) {
+            System.out.println(user);
+        }
+
+        // Очистка таблицы User(ов)
+        userService.cleanUsersTable();
+
+        // Удаление таблицы
+        userService.dropUsersTable();
+
     }
 }
--- src/main/java/jm/task/core/jdbc/model/User.java
+++ src/main/java/jm/task/core/jdbc/model/User.java
@@ -1,21 +1,24 @@
 package jm.task.core.jdbc.model;
 
 import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Table
 public class User {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column
+    @Column(name = "name")
     private String name;
 
-    @Column
+    @Column(name = "lastName")
     private String lastName;
 
-    @Column
+    @Column(name = "age")
     private Byte age;
 
     public User() {
@@ -58,5 +61,13 @@ public class User {
 
     public void setAge(Byte age) {
         this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "User{id=" + id +
+                ", name='" + name + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age + '}';
     }
 }
